71de9bfd53741df49bbdbe92fd0e4bcbed82a3d9
ivopintodasilva <ivopintodasilva@gmail.com>
2015-10-06 15:33:47 +0100

    image upload implemented; not going to static folder;

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from multimedia.models import Image
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    model = Image
+    search_fields = ('name',)
+    list_display= ('name', 'image_thumbnail',)
+
+admin.site.register(Image, ImageAdmin)
--- multimedia/models.py
+++ multimedia/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+def upload_to(instance, filename):
+    return '%s/%s' % (timezone.now().strftime('%d%m%Y'), filename)
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    source_file = models.ImageField(blank=False, upload_to=upload_to)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    caption=models.CharField(max_length=70, blank=True,)
+
+    def clean(self, *args, **kwargs):
+        self.source_file.name = self.source_file.name.encode('ascii','ignore')
+        return super(Image, self).clean(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
+
+    def image_thumbnail(self):
+        return '<img src="%s" width="50" height="50" />' % (self.source_file.url)
+    image_thumbnail.allow_tags = True
