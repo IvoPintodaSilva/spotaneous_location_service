e88746ddd7d1592bcdb4856e43fe60f05cbeadaa
ivopintodasilva <ivopintodasilva@gmail.com>
2015-11-24 14:03:08 +0000

    osmgeoadmin in user

--- custom_users/admin.py
+++ custom_users/admin.py
@@ -6,7 +6,7 @@ from custom_users.models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
 
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(admin.OSMGeoAdmin):
     filter_horizontal = ('interests',)
     
 admin.site.register(CustomUser, CustomUserAdmin)
